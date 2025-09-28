57125f76f322df5ea1906cab2445e5086d0de135
jiseok08 <heoji0912@gmail.com>
2025-09-28 23:56:12 +0900

    (2025.9.26) 물리 시스템

--- Program/Assets/Class 10th (Instantiate)/Scripts/CreateManager.cs
+++ Program/Assets/Class 10th (Instantiate)/Scripts/CreateManager.cs
@@ -1,18 +1,23 @@
+using System.Collections;
 using System.Collections.Generic;
 using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CreateManager : MonoBehaviour
 {
     [SerializeField] int count;
     [SerializeField] float time;
+    [SerializeField] int coroutineCount;
 
     [SerializeField] GameObject prefab;
     [SerializeField] List<GameObject> list;
 
     void Start()
     {
-
+        Create();
+     
+        StartCoroutine(Coroutine());
     }
 
     void Create()
@@ -27,16 +32,22 @@ public class CreateManager : MonoBehaviour
         }
     }
 
-    private void Update()
+    IEnumerator Coroutine()
     {
-        time += Time.deltaTime;
-
-        if (time >= 5.0f)
+        while (coroutineCount < list.Count)
         {
-            Debug.Log("event call");
+            int index = Random.Range(0, list.Count);
 
-            time = 0.0f;
+            yield return new WaitForSeconds(5f);
+
+            if (list[index].activeSelf)
+            {
+                index = (index + 1) % list.Count;
+            }
+
+            list[index].SetActive(true);
+
+            coroutineCount++;
         }
     }
-
 }
